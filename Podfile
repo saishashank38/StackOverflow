704cfef0d23c9b47c1f06c77f12a0661e515fc55
Sai <iossai38@gmail.com>
2020-01-31 13:39:27 +0530

    initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '12.0'
+use_frameworks!
+
+target 'StackOverflow' do
+  pod 'SwiftLint'
+  pod 'NVActivityIndicatorView'
+end
---
+++ StackOverflow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackOverflow
+//
+//  Created by sai on 31/01/20.
+//  Copyright © 2020 sai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
